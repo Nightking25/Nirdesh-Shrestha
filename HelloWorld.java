d6efa78602eb2a1ca80514f881333c441255d681
Nirdesh Shresthaa <sthanirdesh25@gmail.com>
2020-01-31 14:12:19 -0500

    updated HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -21,6 +21,21 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+
+        int a, b;
+        double c;
+
+        a = 5;
+        b = 2;
+
+        a++; //the same as a = a + 1;
+        
+        a += 2;//same as a = a + 2;
+    	a = a / 2;
+        c = ((double) a)/b;
+
+        Syste,.out.println(c++);
+        System.out.println(++c);
     }
 
 }
